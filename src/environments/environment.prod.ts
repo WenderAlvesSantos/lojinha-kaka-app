8b2fd7905b18ec63e83af4f15356e4096ef00064
WenderAlvesSantos <wenderalvesdossantos@hotmail.com>
2025-09-30 22:40:01 -0300

    Update: Configure production API URL and disable SSR

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // Ambiente de produção
 export const environment = {
   production: true,
-  apiUrl: 'https://sua-api-vercel.vercel.app/api' // Atualizar com URL real da Vercel
+  apiUrl: 'https://lojinha-backend-xblh.vercel.app/api'
 };
 
